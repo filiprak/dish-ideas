4f707ea2f314632c15dbcd41211d00eb9423c90e
Filip Rak <filip.rak@ikol.com>
2024-11-16 21:08:28 +0100

    Cleanup

---
+++ src/components/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="layout">
+        <div class="layout__top">
+            <slot name="top"></slot>
+        </div>
+        <div class="layout__content">
+            <slot></slot>
+        </div>
+        <div class="layout__bottom">
+            <slot name="bottom"></slot>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped>
+.layout {
+    display: grid;
+    width: 100%;
+    height: 100%;
+}
+</style>
