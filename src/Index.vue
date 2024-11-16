58fb9026e69b90fdf7164e120d596397f6b86483
Filip Rak <filip.rak@ikol.com>
2024-11-16 21:35:49 +0100

    Cleanup

--- src/Index.vue
+++ src/Index.vue
@@ -4,7 +4,7 @@
     </div>
 </template>
 <script setup lang="ts">
-
+import './index.css';
 </script>
 <style scoped>
 .index {
---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import { Config } from 'tailwindcss';
+
+const config: Partial<Config> = {
+    purge: [],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
+
+export default config;
