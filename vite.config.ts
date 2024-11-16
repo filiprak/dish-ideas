30cca7319bc3f1b692277fccc2f87c0c3b95e6f8
Filip Rak <filip.rak@ikol.com>
2024-11-16 20:01:49 +0100

    Cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,10 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                sw: './src/sw/index.ts',
-                app: './index.html',
-            },
-            output: {
-                entryFileNames(info) {
-                    return info.name == 'sw' ? '[name].js' : 'assets/[name]-[hash].js';
-                },
-            }
-        }
-    },
-    resolve: {
-        alias: {
-            '/sw.js': './src/sw/index.ts',
-        },
-    },
+    plugins: [
+        VitePWA({
+            
+        }),
+    ],
 });
