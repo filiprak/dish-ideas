67b5ac25a4b6ee6d423e7abd7aeafdae89637517
Filip Rak <filip.rak@ikol.com>
2024-11-16 19:56:53 +0100

    Cleanup

--- src/sw/index.ts
+++ src/sw/index.ts
@@ -1,6 +1,7 @@
 /// <reference lib="webworker" />
 
-const CACHE_NAME = `dish-ideas-v1`;
+const VERSION = import.meta.env.VITE_VERSION;
+const CACHE_NAME = `dish-ideas-v${VERSION}`;
 
 function log(msg: string, ...args: unknown[]) {
     console.log(`[sw.js] ${msg}`, ...args)
@@ -11,37 +12,35 @@ self.addEventListener('install', (event: ExtendableEvent) => {
     log('installed', import.meta.env.VITE_VERSION);
 
     event.waitUntil((async () => {
-        const cache = await caches.delete(CACHE_NAME);
-        // const cache = await caches.open(CACHE_NAME);
-        // cache.addAll([
-        //     '/',
-        //     '/converter.js',
-        //     '/converter.css'
-        // ]);
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll([
+            '/',
+        ]);
     })());
 });
 
 self.addEventListener('fetch', (event: FetchEvent) => {
     log('fetch', event.request.url);
 
-    // event.respondWith((async () => {
-    //     const cache = await caches.open(CACHE_NAME);
+    if (event.request)
 
-    //     // Get the resource from the cache.
-    //     const cachedResponse = await cache.match(event.request);
-    //     if (cachedResponse) {
-    //         return cachedResponse;
-    //     } else {
-    //         try {
-    //             // If the resource was not in the cache, try the network.
-    //             const fetchResponse = await fetch(event.request);
+    event.respondWith((async () => {
+        const cache = await caches.open(CACHE_NAME);
 
-    //             // Save the resource in the cache and return it.
-    //             // cache.put(event.request, fetchResponse.clone());
-    //             return fetchResponse;
-    //         } catch (e) {
-    //             // The network failed.
-    //         }
-    //     }
-    // })());
+        // Get the resource from the cache.
+        const cachedResponse = await cache.match(event.request);
+
+        if (cachedResponse) {
+            log('serving cached', event.request.url);
+            return cachedResponse;
+        } else {
+            // If the resource was not in the cache, try the network.
+            const fetchResponse = await fetch(event.request);
+
+            // Save the resource in the cache and return it.
+            cache.put(event.request, fetchResponse.clone());
+
+            return fetchResponse;
+        }
+    })());
 });
