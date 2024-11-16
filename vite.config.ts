eb6ebc87f4e4a9c1cdf59224a08dd4fde37098a5
Filip Rak <filip.rak@ikol.com>
2024-11-16 21:12:16 +0100

    Cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import vue from '@vitejs/plugin-vue';
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
     plugins: [
