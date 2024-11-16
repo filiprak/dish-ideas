a6f5e38b65d03d1a6150e8e2918dfdbcdaa0c391
Filip Rak <filip.rak@ikol.com>
2024-11-16 21:26:51 +0100

    Cleanup

---
+++ src/components/Btn.vue
@@ -0,0 +1,14 @@
+<template>
+    <button class="btn">
+        <slot></slot>
+    </button>
+</template>
+<style scoped>
+.btn {
+    display: inline-grid;
+    outline: none;
+    border: none;
+    padding: 8px 10px;
+    margin: 0;
+}
+</style>
