89f02cfd963b517ebaf54bba5b0e1a2f68c4bf04
Filip Rak <filip.rak@ikol.com>
2024-11-16 21:15:50 +0100

    Cleanup

--- src/Index.vue
+++ src/Index.vue
@@ -1,15 +1,24 @@
 <template>
-    <div class="app">
+    <div class="index">
         <RouterView></RouterView>
     </div>
 </template>
 <script setup lang="ts">
 
 </script>
+<style scoped>
+.index {
+    width: 100%;
+    height: 100%;
+}
+</style>
 <style>
 body,
+#app,
 html {
     padding: 0;
     margin: 0;
+    width: 100%;
+    height: 100%;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
     plugins: [
-        tsconfigPaths(),
+        tsconfigPaths({
+            loose: true,
+        }),
         vue(),
         VitePWA({
 
