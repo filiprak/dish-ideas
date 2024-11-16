b75612d563d6e21923262847bcc4c064825b2aad
Filip Rak <filip.rak@ikol.com>
2024-11-16 21:08:25 +0100

    Cleanup

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>home</div>
+    <Layout class="home">
+        <template #bottom>
+            
+        </template>
+    </Layout>
 </template>
 <script setup lang="ts">
+import Layout from '@/components/Layout.vue';
 
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import vue from '@vitejs/plugin-vue';
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
     plugins: [
+        tsconfigPaths(),
         vue(),
         VitePWA({
 
