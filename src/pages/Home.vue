d6466edf7e9e0f95ca669fc64b24956ca7a74cea
Filip Rak <filip.rak@ikol.com>
2024-11-16 21:26:48 +0100

    Cleanup

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,12 @@
 <template>
     <Layout class="home">
         <template #bottom>
-            
+            <Btn>test</Btn>
         </template>
     </Layout>
 </template>
 <script setup lang="ts">
 import Layout from '@/components/Layout.vue';
+import Btn from '@/components/Btn.vue';
 
 </script>
