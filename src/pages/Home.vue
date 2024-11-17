9f8669e1c234216680ef1b10e827f83bba595ac4
Filip Rak <filip.rak@ikol.com>
2024-11-17 21:21:44 +0100

    Cleanup

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,9 +7,23 @@
             </div>
         </template>
         <div class="p-4">
-            <!-- <Md :source="recipes[0]" /> -->
-
-            {{ ideas }}
+            <div class="space-y-4 text-left text-gray-500 dark:text-gray-400">
+                <div v-for="idea in ideas"
+                     class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400"
+                         aria-hidden="true"
+                         xmlns="http://www.w3.org/2000/svg"
+                         fill="none"
+                         viewBox="0 0 16 12">
+                        <path stroke="currentColor"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="2"
+                              d="M1 5.917 5.724 10.5 15 1.5" />
+                    </svg>
+                    <span>{{ idea }}</span>
+                </div>
+            </div>
         </div>
         <template #bottom>
             <Btn @click="load"
